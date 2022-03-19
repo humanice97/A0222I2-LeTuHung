631a7440a177a605b79dc68133a1a943ac7556a6
humanice97 <humanice97@gmail.com>
2022-03-19 19:28:57 +0700

    nop bai tap ss7

---
+++ Module1/ss7_bien_kieudulieu_toantu/thuc_hanh/toantu/bai1.js
@@ -0,0 +1,11 @@
+// let inputWidth;
+// let inputHeight;
+
+let inputHeight = prompt("Nhập chiều cao");
+let inputWidth = prompt("Nhập chiều rộng");
+
+let height = parseInt(inputHeight);
+let width = parseInt(inputWidth);
+
+let area = width * height;
+document.write("Diện tích = " + area);
