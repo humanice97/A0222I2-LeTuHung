7441dad433c9864be4b22001dced1f185f60d326
humanice97 <humanice97@gmail.com>
2022-07-23 22:09:36 +0700

    update

---
+++ Module3/data_base/xaydung_csdl_quanly_sv.sql
@@ -0,0 +1,18 @@
+create database my_database;
+use my_database;
+create table Class(id int auto_increment primary key not null
+,name varchar(50));
+create table Teacher(id int
+, name varchar(50)
+, age int 
+, country varchar(50));
+
+insert into Class(name) values("Le Tu Hung");
+insert into Class(name) values("Le Tu Hung1");
+insert into Class(name) values("Le Tu Hung2");
+insert into Class(name) values("Le Tu Hung3");
+insert into Class(name) values("Le Tu Hung4");
+
+
+insert into Teacher (name,age,country) values("Le Tu Hung5","18","Viet Nam");
+insert into Teacher (name,age,country) values("Le Tu Hung6","22","My Tho");
